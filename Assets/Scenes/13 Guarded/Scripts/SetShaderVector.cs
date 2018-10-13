5c2b680bd713626f89604d709c562ce7abe6cc2a
Remy Sohier <remy.sohier@gmail.com>
2018-10-13 17:53:00 +0200

    13 Guarded wip
    Signed-off-by: Remy Sohier <remy.sohier@gmail.com>

---
+++ Assets/Scenes/13 Guarded/Scripts/SetShaderVector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SetShaderVector : MonoBehaviour {
+
+    public Transform targetTransformToCopy;
+    public string variableName;
+    Renderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    void Update () {
+        if (variableName == "")
+            return;
+
+        _renderer.sharedMaterial.SetVector(variableName, targetTransformToCopy.position);
+    }
+}
